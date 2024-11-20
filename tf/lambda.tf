38f088743eac4c55e5b5c182a3a424221fe06147
dickravison <78479145+dickravison@users.noreply.github.com>
2024-11-20 20:20:50 +0000

    fix: adding missing eventbridge permission

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -16,3 +16,11 @@ resource "aws_lambda_function" "euromillions" {
     }
   }
 }
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventbridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.euromillions.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.euromillions.arn
+}
